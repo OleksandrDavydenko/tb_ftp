076c115a9766d7c7c159e25dee49ef610d68d88d
oleksandrdavydenko <oleksandr.davydenko3@gmail.com>
2024-10-30 16:30:57 +0200

    301024/1

--- key.py
+++ key.py
@@ -1,3 +1,7 @@
-KEY = '7680784394:AAEe5BxQpV70HnPWDepZsRWwZByfosFFiII'
+import os
+KEY = os.getenv('TELEGRAM_BOT_TOKEN')
+
+
+""" KEY = '7680784394:AAEe5BxQpV70HnPWDepZsRWwZByfosFFiII' """
 
 """ KEY ='7673923226:AAFaAV2eGObx30fiYGhn7psEWXH4ct6Smuw' """
