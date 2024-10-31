da049bec1c9fe431ea77f3b9c3d8ffb060056ed6
oleksandrdavydenko <oleksandr.davydenko3@gmail.com>
2024-10-31 12:57:53 +0200

    311024/31

--- telegrambot.py
+++ telegrambot.py
@@ -4,15 +4,13 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import logging
 import os
-import signal
 import sys
-import atexit
 
-from messages.check_payments import run_periodic_check
+from messages.check_payments import check_new_payments
+from messages.sync_payments import sync_payments
+from messages.reminder import schedule_monthly_reminder
 from auth import is_phone_number_in_power_bi
 from db import add_telegram_user, get_user_joined_at
-from messages.sync_payments import sync_payments, run_periodic_sync
-from messages.reminder import schedule_monthly_reminder
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from deb.debt_handlers import show_debt_options, show_debt_details, show_debt_histogram, show_debt_pie_chart
@@ -22,60 +20,36 @@ KEY = os.getenv('TELEGRAM_BOT_TOKEN')
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Ініціалізуємо планувальник як глобальну змінну
 scheduler = AsyncIOScheduler()
 
 async def start(update: Update, context: CallbackContext) -> None:
     context.user_data['registered'] = False
-    context.user_data['phone_number'] = None
     await prompt_for_phone_number(update, context)
 
 async def prompt_for_phone_number(update: Update, context: CallbackContext) -> None:
     contact_button = KeyboardButton(text="Поділитися номером телефоном", request_contact=True)
-    custom_keyboard = [[contact_button]]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True)
+    reply_markup = ReplyKeyboardMarkup([[contact_button]], one_time_keyboard=True)
     await update.message.reply_text("Будь ласка, поділіться своїм номером телефону:", reply_markup=reply_markup)
-
-def normalize_phone_number(phone_number):
-    if phone_number.startswith('+'):
-        phone_number = phone_number[1:]
-    return phone_number
 
 async def handle_contact(update: Update, context: CallbackContext) -> None:
     if update.message.contact:
-        phone_number = normalize_phone_number(update.message.contact.phone_number)
-        logging.info(f"Отримано номер телефону: {phone_number}")
+        phone_number = update.message.contact.phone_number.lstrip('+')
         found, employee_name = is_phone_number_in_power_bi(phone_number)
-        
+
         if found:
-            logging.info(f"Користувач знайдений: {employee_name}")
             add_telegram_user(phone_number=phone_number, telegram_id=update.message.from_user.id,
-                              telegram_name=update.message.from_user.first_name, employee_name=employee_name)
-            joined_at = get_user_joined_at(phone_number)
-            logging.info(f"Дата приєднання користувача: {joined_at}")
-
-            if joined_at:
-                try:
-                    await sync_payments()
-                except Exception as e:
-                    logging.error(f"Помилка при синхронізації платежів: {e}")
-
-            context.user_data['registered'] = True
-            context.user_data['phone_number'] = phone_number
-            context.user_data['telegram_name'] = update.message.from_user.first_name
-            context.user_data['employee_name'] = employee_name
-            await update.message.reply_text(f"Вітаємо, {context.user_data['employee_name']}! Доступ надано.")
+                              employee_name=employee_name)
+            context.user_data.update({'registered': True, 'phone_number': phone_number, 'employee_name': employee_name})
+            await update.message.reply_text(f"Вітаємо, {employee_name}! Доступ надано.")
             await show_main_menu(update, context)
         else:
             await update.message.reply_text("Ваш номер не знайдено. Доступ заборонено.")
             await prompt_for_phone_number(update, context)
 
 async def show_main_menu(update: Update, context: CallbackContext) -> None:
-    context.user_data['menu'] = 'main_menu'
     debt_button = KeyboardButton(text="Дебіторка")
     salary_button = KeyboardButton(text="Розрахунковий лист")
-    custom_keyboard = [[debt_button, salary_button]]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True)
+    reply_markup = ReplyKeyboardMarkup([[debt_button, salary_button]], one_time_keyboard=True)
     await update.message.reply_text("Виберіть опцію:", reply_markup=reply_markup)
 
 async def handle_main_menu(update: Update, context: CallbackContext) -> None:
@@ -85,40 +59,21 @@ async def handle_main_menu(update: Update, context: CallbackContext) -> None:
 
     if update.message.text == "Дебіторка":
         await show_debt_options(update, context)
-
     elif update.message.text == "Таблиця":
         await show_debt_details(update, context)
-
     elif update.message.text == "Гістограма":
         await show_debt_histogram(update, context)
-
     elif update.message.text == "Діаграма":
         await show_debt_pie_chart(update, context)
-
-    elif update.message.text == "Назад":
-        current_menu = context.user_data.get('menu')
-        if current_menu == 'salary_months':
-            await show_salary_years(update, context)
-        elif current_menu == 'salary_years':
-            await show_main_menu(update, context)
-        elif current_menu == 'debt_options':
-            await show_main_menu(update, context)
-        elif current_menu in ['debt_details', 'debt_histogram', 'debt_pie_chart']:
-            await show_debt_options(update, context)
-        else:
-            await show_main_menu(update, context)
-
     elif update.message.text == "Розрахунковий лист":
         await show_salary_years(update, context)
-
     elif update.message.text == "Головне меню":
         await show_main_menu(update, context)
-
     elif update.message.text in ["2024", "2025"]:
         context.user_data['selected_year'] = update.message.text
         await show_salary_months(update, context)
-
-    elif update.message.text in ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"]:
+    elif update.message.text in ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень",
+                                 "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"]:
         context.user_data['selected_month'] = update.message.text
         await show_salary_details(update, context)
 
@@ -129,14 +84,13 @@ async def shutdown(app, scheduler):
     logging.info("Планувальник зупинено.")
 
 def main():
-    token = KEY
-    app = ApplicationBuilder().token(token).build()
+    app = ApplicationBuilder().token(KEY).build()
     scheduler = AsyncIOScheduler()
 
     # Налаштування запланованих завдань
-    scheduler.add_job(run_periodic_check, 'interval', minutes=1, max_instances=1, misfire_grace_time=60)
-    scheduler.add_job(run_periodic_sync, 'interval', minutes=1, max_instances=1, misfire_grace_time=60)
-    schedule_monthly_reminder(scheduler)  # Додаємо нагадування до планувальника
+    scheduler.add_job(check_new_payments, 'interval', seconds=60, max_instances=1, misfire_grace_time=30)
+    scheduler.add_job(sync_payments, 'interval', seconds=60, max_instances=1, misfire_grace_time=30)
+    schedule_monthly_reminder(scheduler)
 
     scheduler.start()
     logging.info("Планувальник запущено.")
@@ -144,9 +98,11 @@ def main():
     # Обробники команд та повідомлень
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.CONTACT, handle_contact))
-    app.add_handler(MessageHandler(filters.Regex("^(Дебіторка|Назад|Таблиця|Гістограма|Діаграма|Розрахунковий лист|Головне меню|2024|2025|Січень|Лютий|Березень|Квітень|Травень|Червень|Липень|Серпень|Вересень|Жовтень|Листопад|Грудень)$"), handle_main_menu))
+    app.add_handler(MessageHandler(filters.Regex(
+        "^(Дебіторка|Назад|Таблиця|Гістограма|Діаграма|Розрахунковий лист|Головне меню|2024|2025|"
+        "Січень|Лютий|Березень|Квітень|Травень|Червень|Липень|Серпень|Вересень|Жовтень|Листопад|Грудень)$"), 
+        handle_main_menu))
 
-    # Запускаємо бота в циклі подій
     try:
         app.run_polling()
     finally:
@@ -154,5 +110,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    
