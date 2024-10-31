f46add16664a9f84f6feb29c88666aee4da0a04a
oleksandrdavydenko <oleksandr.davydenko3@gmail.com>
2024-10-31 11:03:05 +0200

    311224/13

--- telegrambot.py
+++ telegrambot.py
@@ -1,27 +1,23 @@
-import asyncio  # Додаємо імпорт asyncio
+import asyncio
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, CallbackContext
-import threading
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import logging
+import os
+import sys
+
 from messages.check_payments import run_periodic_check
 from auth import is_phone_number_in_power_bi
 from db import add_telegram_user, get_user_joined_at
-from messages.sync_payments import sync_payments
-from messages.sync_payments import run_periodic_sync  # Імпорт періодичної синхронізації
+from messages.sync_payments import sync_payments, run_periodic_sync
 from messages.reminder import schedule_monthly_reminder
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-import sys
-import os
-import logging
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from deb.debt_handlers import show_debt_options, show_debt_details, show_debt_histogram, show_debt_pie_chart
+from salary.salary_handlers import show_salary_years, show_salary_months, show_salary_details
 
 KEY = os.getenv('TELEGRAM_BOT_TOKEN')
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from deb.debt_handlers import show_debt_options, show_debt_details, show_debt_histogram, show_debt_pie_chart, show_main_menu
-from salary.salary_handlers import show_salary_years, show_salary_months, show_salary_details
-
-# Налаштування логування
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -35,9 +31,7 @@ async def prompt_for_phone_number(update: Update, context: CallbackContext) -> N
     reply_markup = ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True)
     await update.message.reply_text("Будь ласка, поділіться своїм номером телефону:", reply_markup=reply_markup)
 
-
 def normalize_phone_number(phone_number):
-    # Видаляємо '+' на початку номера
     if phone_number.startswith('+'):
         phone_number = phone_number[1:]
     return phone_number
@@ -45,45 +39,32 @@ def normalize_phone_number(phone_number):
 async def handle_contact(update: Update, context: CallbackContext) -> None:
     if update.message.contact:
         phone_number = normalize_phone_number(update.message.contact.phone_number)
-
         logging.info(f"Отримано номер телефону: {phone_number}")
         found, employee_name = is_phone_number_in_power_bi(phone_number)
         
-
         if found:
             logging.info(f"Користувач знайдений: {employee_name}")
-
-            # Додаємо користувача в базу даних з новими назвами стовпців
-            add_telegram_user(
-                phone_number=phone_number,
-                telegram_id=update.message.from_user.id,
-                telegram_name=update.message.from_user.first_name,
-                employee_name=employee_name
-                #employee_name=update.message.from_user.last_name
-            )
-
-            # Отримуємо дату приєднання користувача
+            add_telegram_user(phone_number=phone_number, telegram_id=update.message.from_user.id,
+                              telegram_name=update.message.from_user.first_name, employee_name=employee_name)
             joined_at = get_user_joined_at(phone_number)
             logging.info(f"Дата приєднання користувача: {joined_at}")
 
             if joined_at:
                 try:
-                    await sync_payments()  # Додаємо await
+                    await sync_payments()
                 except Exception as e:
                     logging.error(f"Помилка при синхронізації платежів: {e}")
 
-            # Оновлюємо дані користувача в контексті
             context.user_data['registered'] = True
             context.user_data['phone_number'] = phone_number
             context.user_data['telegram_name'] = update.message.from_user.first_name
             context.user_data['employee_name'] = employee_name
-
-            # Вітання та відображення головного меню
             await update.message.reply_text(f"Вітаємо, {context.user_data['employee_name']}! Доступ надано.")
             await show_main_menu(update, context)
         else:
             await update.message.reply_text("Ваш номер не знайдено. Доступ заборонено.")
             await prompt_for_phone_number(update, context)
+
 async def show_main_menu(update: Update, context: CallbackContext) -> None:
     context.user_data['menu'] = 'main_menu'
     debt_button = KeyboardButton(text="Дебіторка")
@@ -136,26 +117,19 @@ async def handle_main_menu(update: Update, context: CallbackContext) -> None:
         context.user_data['selected_month'] = update.message.text
         await show_salary_details(update, context)
 
-
-
 def main():
     token = KEY
     app = ApplicationBuilder().token(token).build()
-
+    
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(run_periodic_check, 'interval', minutes=30)
+    scheduler.add_job(run_periodic_sync, 'interval', minutes=60)
+    scheduler.add_job(schedule_monthly_reminder, 'interval', weeks=4)
+    scheduler.start()
+    
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.CONTACT, handle_contact))
     app.add_handler(MessageHandler(filters.Regex("^(Дебіторка|Назад|Таблиця|Гістограма|Діаграма|Розрахунковий лист|Головне меню|2024|2025|Січень|Лютий|Березень|Квітень|Травень|Червень|Липень|Серпень|Вересень|Жовтень|Листопад|Грудень)$"), handle_main_menu))
-
-    # Створюємо окремий потік для перевірки нових виплат
-
-    check_thread = threading.Thread(target=lambda: asyncio.run(run_periodic_check()))
-    sync_thread = threading.Thread(target=lambda: asyncio.run(run_periodic_sync()))
-    check_thread.daemon = True
-    sync_thread.daemon = True
-    check_thread.start()
-    sync_thread.start()
-
-    
 
     app.run_polling()
 
