3f0a18c1fbbdcebbb6dec2dd70ff4eb62d89ffe2
oleksandrdavydenko <oleksandr.davydenko3@gmail.com>
2024-10-31 13:06:26 +0200

    311024/32

--- telegrambot.py
+++ telegrambot.py
@@ -8,9 +8,9 @@ import sys
 
 from messages.check_payments import check_new_payments
 from messages.sync_payments import sync_payments
-from messages.reminder import schedule_monthly_reminder
 from auth import is_phone_number_in_power_bi
 from db import add_telegram_user, get_user_joined_at
+from messages.reminder import schedule_monthly_reminder
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from deb.debt_handlers import show_debt_options, show_debt_details, show_debt_histogram, show_debt_pie_chart
@@ -31,16 +31,35 @@ async def prompt_for_phone_number(update: Update, context: CallbackContext) -> N
     reply_markup = ReplyKeyboardMarkup([[contact_button]], one_time_keyboard=True)
     await update.message.reply_text("Будь ласка, поділіться своїм номером телефону:", reply_markup=reply_markup)
 
+def normalize_phone_number(phone_number):
+    return phone_number[1:] if phone_number.startswith('+') else phone_number
+
 async def handle_contact(update: Update, context: CallbackContext) -> None:
     if update.message.contact:
-        phone_number = update.message.contact.phone_number.lstrip('+')
+        phone_number = normalize_phone_number(update.message.contact.phone_number)
+        logging.info(f"Отримано номер телефону: {phone_number}")
         found, employee_name = is_phone_number_in_power_bi(phone_number)
-
+        
         if found:
+            logging.info(f"Користувач знайдений: {employee_name}")
             add_telegram_user(phone_number=phone_number, telegram_id=update.message.from_user.id,
-                              employee_name=employee_name)
-            context.user_data.update({'registered': True, 'phone_number': phone_number, 'employee_name': employee_name})
-            await update.message.reply_text(f"Вітаємо, {employee_name}! Доступ надано.")
+                              telegram_name=update.message.from_user.first_name, employee_name=employee_name)
+            joined_at = get_user_joined_at(phone_number)
+            logging.info(f"Дата приєднання користувача: {joined_at}")
+
+            if joined_at:
+                try:
+                    await sync_payments()
+                except Exception as e:
+                    logging.error(f"Помилка при синхронізації платежів: {e}")
+
+            context.user_data.update({
+                'registered': True,
+                'phone_number': phone_number,
+                'telegram_name': update.message.from_user.first_name,
+                'employee_name': employee_name
+            })
+            await update.message.reply_text(f"Вітаємо, {context.user_data['employee_name']}! Доступ надано.")
             await show_main_menu(update, context)
         else:
             await update.message.reply_text("Ваш номер не знайдено. Доступ заборонено.")
@@ -57,39 +76,47 @@ async def handle_main_menu(update: Update, context: CallbackContext) -> None:
         await prompt_for_phone_number(update, context)
         return
 
-    if update.message.text == "Дебіторка":
+    text = update.message.text
+    if text == "Дебіторка":
         await show_debt_options(update, context)
-    elif update.message.text == "Таблиця":
+    elif text == "Таблиця":
         await show_debt_details(update, context)
-    elif update.message.text == "Гістограма":
+    elif text == "Гістограма":
         await show_debt_histogram(update, context)
-    elif update.message.text == "Діаграма":
+    elif text == "Діаграма":
         await show_debt_pie_chart(update, context)
-    elif update.message.text == "Розрахунковий лист":
+    elif text == "Назад":
+        menu = context.user_data.get('menu')
+        if menu == 'salary_months':
+            await show_salary_years(update, context)
+        elif menu == 'salary_years' or menu == 'debt_options':
+            await show_main_menu(update, context)
+        elif menu in ['debt_details', 'debt_histogram', 'debt_pie_chart']:
+            await show_debt_options(update, context)
+        else:
+            await show_main_menu(update, context)
+    elif text == "Розрахунковий лист":
         await show_salary_years(update, context)
-    elif update.message.text == "Головне меню":
+    elif text == "Головне меню":
         await show_main_menu(update, context)
-    elif update.message.text in ["2024", "2025"]:
-        context.user_data['selected_year'] = update.message.text
+    elif text in ["2024", "2025"]:
+        context.user_data['selected_year'] = text
         await show_salary_months(update, context)
-    elif update.message.text in ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень",
-                                 "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"]:
-        context.user_data['selected_month'] = update.message.text
+    elif text in ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"]:
+        context.user_data['selected_month'] = text
         await show_salary_details(update, context)
 
 async def shutdown(app, scheduler):
     await app.shutdown()
-    logging.info("Зупиняємо планувальник...")
     scheduler.shutdown(wait=True)
     logging.info("Планувальник зупинено.")
 
 def main():
     app = ApplicationBuilder().token(KEY).build()
-    scheduler = AsyncIOScheduler()
 
-    # Налаштування запланованих завдань
-    scheduler.add_job(check_new_payments, 'interval', seconds=60, max_instances=1, misfire_grace_time=30)
-    scheduler.add_job(sync_payments, 'interval', seconds=60, max_instances=1, misfire_grace_time=30)
+    # Додаємо завдання до планувальника
+    scheduler.add_job(check_new_payments, 'interval', seconds=60)
+    scheduler.add_job(sync_payments, 'interval', seconds=60)
     schedule_monthly_reminder(scheduler)
 
     scheduler.start()
@@ -98,11 +125,9 @@ def main():
     # Обробники команд та повідомлень
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.CONTACT, handle_contact))
-    app.add_handler(MessageHandler(filters.Regex(
-        "^(Дебіторка|Назад|Таблиця|Гістограма|Діаграма|Розрахунковий лист|Головне меню|2024|2025|"
-        "Січень|Лютий|Березень|Квітень|Травень|Червень|Липень|Серпень|Вересень|Жовтень|Листопад|Грудень)$"), 
-        handle_main_menu))
+    app.add_handler(MessageHandler(filters.Regex("^(Дебіторка|Назад|Таблиця|Гістограма|Діаграма|Розрахунковий лист|Головне меню|2024|2025|Січень|Лютий|Березень|Квітень|Травень|Червень|Липень|Серпень|Вересень|Жовтень|Листопад|Грудень)$"), handle_main_menu))
 
+    # Запускаємо бота в циклі подій
     try:
         app.run_polling()
     finally:
