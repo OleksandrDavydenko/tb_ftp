bfe5864a6b1277e06c58fb825b69c99d98421d07
oleksandrdavydenko <oleksandr.davydenko3@gmail.com>
2024-11-15 14:41:56 +0200

    151124/6

--- db.py
+++ db.py
@@ -148,22 +148,3 @@ create_tables()
 
 
 
-
-
-def delete_all_records_in_devaluationanalysis():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    
-    try:
-        # Видаляємо всі записи з таблиці DevaluationAnalysis
-        cursor.execute("DELETE FROM DevaluationAnalysis")
-        conn.commit()
-        logging.info("Всі записи з таблиці DevaluationAnalysis успішно видалено.")
-    except Exception as e:
-        logging.error(f"Помилка при видаленні записів з таблиці DevaluationAnalysis: {e}")
-    finally:
-        cursor.close()
-        conn.close()
-
-# Викликаємо функцію для видалення всіх записів
-delete_all_records_in_devaluationanalysis()
