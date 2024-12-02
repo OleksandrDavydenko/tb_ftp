9b7f551b2963c6c4612b0b34cee11b7b03cd61e8
oleksandrdavydenko <oleksandr.davydenko3@gmail.com>
2024-12-02 10:57:41 +0200

    021224/5

--- telegrambot.py
+++ telegrambot.py
@@ -196,7 +196,7 @@ def main():
         store_exchange_rates,
         'cron',
         hour=10,
-        minute=58,
+        minute=61,
         timezone=kyiv_timezone,
         id='daily_exchange_rates',
     )
