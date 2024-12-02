e102e999038773864d0a79c44acb7852b81c63fd
oleksandrdavydenko <oleksandr.davydenko3@gmail.com>
2024-12-02 13:12:15 +0200

    021224/9

--- information/help_menu.py
+++ information/help_menu.py
@@ -1,6 +1,6 @@
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import CallbackContext
-from information.currency_query import get_latest_currency_rates
+from db import get_latest_currency_rates
 
 async def show_help_menu(update: Update, context: CallbackContext) -> None:
     """
@@ -13,18 +13,14 @@ async def show_help_menu(update: Update, context: CallbackContext) -> None:
 
 async def show_currency_rates(update: Update, context: CallbackContext) -> None:
     """
-    Відображає останні курси валют USD і EUR з бази даних.
+    Отримує та відображає останні курси валют з бази даних.
     """
     try:
-        rates = get_latest_currency_rates()
-        if rates:
-            usd_rate, eur_rate = rates
-            await update.message.reply_text(
-                f"📈 Курси валют:\n\n"
-                f"💵 USD: {usd_rate['rate']} (оновлено: {usd_rate['timestamp']})\n"
-                f"💶 EUR: {eur_rate['rate']} (оновлено: {eur_rate['timestamp']})"
-            )
-        else:
-            await update.message.reply_text("Не вдалося отримати курси валют. Спробуйте пізніше.")
+        rates = get_latest_currency_rates(["USD", "EUR"])  # Отримати з БД
+        message = "Курси валют:\n"
+        for rate in rates:
+            message += f"{rate['currency']}: {rate['rate']} (оновлено: {rate['timestamp']})\n"
+        await update.message.reply_text(message)
     except Exception as e:
-        await update.message.reply_text(f"Помилка: {e}")
+        await update.message.reply_text("Не вдалося отримати курси валют. Спробуйте пізніше.")
+        raise e
