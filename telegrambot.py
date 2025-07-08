c9f3161b00c59aa5538b79f22135b43ff45c874e
Oleksandr <59095012+OleksandrDavydenko@users.noreply.github.com>
2025-07-08 07:02:59 +0000

    080725/1

--- telegrambot.py
+++ telegrambot.py
@@ -508,7 +508,7 @@ def main():
 
 
 
-    scheduler.add_job(send_birthday_greetings, 'cron', hour=14, minute=12, timezone='Europe/Kiev')
+    scheduler.add_job(send_birthday_greetings, 'cron', hour=10, minute=10, timezone='Europe/Kiev')
     
 
 
