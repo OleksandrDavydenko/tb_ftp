8673568cc6ada259c9f8788a2caeb2df64db1a8f
Oleksandr <59095012+OleksandrDavydenko@users.noreply.github.com>
2025-09-09 09:02:41 +0000

    090925/4

--- telegrambot.py
+++ telegrambot.py
@@ -468,8 +468,8 @@ def main():
     scheduler.add_job(
         store_exchange_rates,
         'cron',
-        hour=11,
-        minute=59,
+        hour=10,
+        minute=05,
         timezone=kyiv_timezone,
         id='daily_exchange_rates',
     )
