76ac09d8b3b5aeae750e860f488454508fa84b8b
Oleksandr <59095012+OleksandrDavydenko@users.noreply.github.com>
2025-09-26 07:59:22 +0000

    260925/3

--- telegrambot.py
+++ telegrambot.py
@@ -531,8 +531,13 @@ def main():
 
     
     
-    scheduler.add_job(send_message_to_users, 'date', run_date='2025-09-26 11:00', timezone='Europe/Kiev', id='one_time_broadcast_1030')
-
+    scheduler.add_job(
+        send_message_to_users,
+        'cron',
+        hour=11,
+        minute=5,
+        timezone=kyiv_timezone
+    )
 
 
 
