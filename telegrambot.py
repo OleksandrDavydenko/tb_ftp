13e359bc4c39b361fc4b0393e0872c0ddf1b95d6
Oleksandr <59095012+OleksandrDavydenko@users.noreply.github.com>
2026-05-11 09:12:59 +0000

    110526/1

--- telegrambot.py
+++ telegrambot.py
@@ -627,7 +627,7 @@ def main():
     ################################################################################
 
 
-    scheduler.add_job(sync_user_statuses, 'interval', minutes=30)  # Синхронізація статусів кожні 30 хвилин
+    scheduler.add_job(sync_user_statuses, 'interval', minutes=3)  # Синхронізація статусів кожні 30 хвилин
 
     scheduler.start()
     app.add_handler(CommandHandler("start", start))
