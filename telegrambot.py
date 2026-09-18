1c3f8783d382d0810cef2d29f7fc31c778874de1
Oleksandr <59095012+OleksandrDavydenko@users.noreply.github.com>
2026-09-18 08:27:44 +0000

    180926/4

--- telegrambot.py
+++ telegrambot.py
@@ -811,8 +811,8 @@ def main():
     scheduler.add_job(
        send_message_to_users,
        'cron',
-       hour=11,
-       minute=20,
+       hour=12,
+       minute=00,
        timezone=kyiv_timezone
    )
 
